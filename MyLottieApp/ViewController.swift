c67695fa9f7f6e32a801b3e2b6715256ef310092
Marcos Garcia <marcos.x.garcia.-nd@disney.com>
2018-03-13 14:08:48 -0700

    Initial Commit

---
+++ MyLottieApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyLottieApp
+//
+//  Created by marcosgn1 on 3/13/18.
+//  Copyright © 2018 Disneyland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
